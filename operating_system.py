dc911da2a4f738bae0565db8120e0a412e6def34
Mohamad Reza <m.reza.s.moghadam@gmail.com>
2025-02-23 15:20:50 +0330

    🔧 adding the method to init the attributes of Folder class

--- operating_system.py
+++ operating_system.py
@@ -1,11 +1,17 @@
 #new file is here
 #this is the develop branch
 
-class File:
-    def __init__():
-        pass
-
 
 class Folder:
-    def __init__():
+    def __init__(self, file : File, folder : Folder):
+        self._file = file
+        self._folder = folder   
+
+
+
+class File:
+    def __init__(self,):
         pass
+
+
+
