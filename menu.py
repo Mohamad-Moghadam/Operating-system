7237b2b4f40c30867a754d086380255611449bd0
Mohamad Reza <m.reza.s.moghadam@gmail.com>
2025-04-05 22:03:24 +0330

    🐛 almost perfect

--- menu.py
+++ menu.py
@@ -2,11 +2,24 @@ from operating_system import Tree, Commands
 
 
 def menu():
+    cmd = Commands(None, "root", None)
+
     while True:
         command = input("What do you want to do?\n1. mkdir\n2. cd\n3. rm\n4. ls")
-        if command == 1:
+        if command == "1":
             name= input(f"folder name: ")
-            Commands.mkdir(name)
-        elif command == 2:
+            cmd.mkdir(name)
+
+        elif command == "2":
             dir= input(f"desired dir: ")
-            Commands.cd(dir)
+            cmd.cd(dir)
+
+        elif command == "3":
+            des= input(f"which dir: ")
+            cmd.rm(des)
+
+        elif command == "4":
+            cmd.ls()
+
+menu()
+
